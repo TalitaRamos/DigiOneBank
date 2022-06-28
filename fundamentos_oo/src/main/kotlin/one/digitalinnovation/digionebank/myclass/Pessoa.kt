0ef3da21c6c49b2bb27f01e885c7e9fb30527e6c
Talit <talitasantos.hrzon@gmail.com>
2022-06-27 23:38:24 -0300

    Criando classe pessoa

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/myclass/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank.myclass
+
+class Pessoa {
+    var nome: String = "Talita"
+    var cpf: String = "123.456.789-00"
+}
+
+fun main() {
+    val talita = Pessoa()
+
+    println(talita.nome)
+    println(talita.cpf)
+}
