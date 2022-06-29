d5bedbb34f6033742aa4bceb321a3650a6710b66
Talit <talitasantos.hrzon@gmail.com>
2022-06-29 00:31:04 -0300

    Tornar classes abstratas

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/myclass/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/myclass/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.digionebank.myclass
 
-class Pessoa {
-    var nome: String = "Talita"
-
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val talita = Pessoa()
-
-    println(talita.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome : String,
+    val cpf : String
+)
